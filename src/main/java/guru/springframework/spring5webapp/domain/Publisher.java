5403907b5f548293bd8e5857685cc69236d6e4c0
staszko <30053337+staszko@users.noreply.github.com>
2021-04-03 15:42:05 +0200

    (Assignment 2) Publishe entity

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,104 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String addressFirstLine;
+    private String city;
+    private String sate;
+    private String zipCode;
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String addressFirstLine, String city, String sate, String zipCode) {
+        this.name = name;
+        this.addressFirstLine = addressFirstLine;
+        this.city = city;
+        this.sate = sate;
+        this.zipCode = zipCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressFirstLine() {
+        return addressFirstLine;
+    }
+
+    public void setAddressFirstLine(String addressFirstLine) {
+        this.addressFirstLine = addressFirstLine;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSate() {
+        return sate;
+    }
+
+    public void setSate(String sate) {
+        this.sate = sate;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Publisher.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("addressFirstLine='" + addressFirstLine + "'")
+                .add("city='" + city + "'")
+                .add("sate='" + sate + "'")
+                .add("zipCode='" + zipCode + "'")
+                .toString();
+    }
+}
